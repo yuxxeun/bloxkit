20a25a753214bce365adae4ee17e61cfb86a4fd8
yuxxeun <itsyuxxeun@gmail.com>
2026-06-09 08:59:11 +0700

    chore: add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <Analytics />
     </ThemeProvider>
   </StrictMode>
 )
